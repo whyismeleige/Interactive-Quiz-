37aca135039b3ea1d6c9a292695d0db588444e1e
whyismeleige <piyushjain31456@gmail.com>
2025-02-11 22:37:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+//Quiz List
+const quiz = {
+  question1: {
+    Question: `What is the brain of the Computer?`,
+    Option1: `Hard Drive`,
+    Option2: `Monitor`,
+    Option3: `CPU`,
+    Option4: `Keyboard`,
+  },
+  question2: {
+    Question: `One purpose of a network is?`,
+    Option1: `Sharing Software`,
+    Option2: `Compiling Program`,
+    Option3: `Changing Software`,
+    Option4: `Starting up the Computer`,
+  },
+  question3: {
+    Question: `The Miss Porters Network is`,
+    Option1: `MAN`,
+    Option2: `SAN`,
+    Option3: `LAN`,
+    Option4: `WAN`,
+  },
+  question4: {
+    Question: `What Program do you run to enter the Python editor?`,
+    Option1: `Windows`,
+    Option2: `Python`,
+    Option3: `IDLE`,
+    Option4: `Interactive Mode`,
+  },
+  question5: {
+    Question: `The Computer Malware that disguises users by representing itself as legitimate software/program is known as ?`,
+    Option1: `Virus`,
+    Option2: `Worms`,
+    Option3: `Trojans`,
+    Option4: `Spyware`,
+  },
+  question6: {
+    Question: `The Second Generation Computer used ?`,
+    Option1: `Transistors`,
+    Option2: `Integrated Circuit`,
+    Option3: `Vacuum Tube`,
+    Option4: `Chip`,
+  },
+  question7: {
+    Question: `To store data and perform calculation, computer uses ....... number system.`,
+    Option1: `Hexadecimal`,
+    Option2: `Octadecimal`,
+    Option3: `Binary`,
+    Option4: `Decimal`,
+  },
+  question8: {
+    Question: `A Web Address is usually known as ...`,
+    Option1: `URL`,
+    Option2: `UWL`,
+    Option3: `WWW`,
+    Option4: `UVL`,
+  },
+  question9: {
+    Question: `One of the major works of software is to transform data into .....`,
+    Option1: `Information`,
+    Option2: `Video`,
+    Option3: `Website`,
+    Option4: `Software Program`,
+  },
+  question10: {
+    Question: `Which among the following is not correctly defines a functional domain name?`,
+    Option1: `.net`,
+    Option2: `.org`,
+    Option3: `.gov`,
+    Option4: `.god`,
+  },
+};
+
+// for(let question in quiz){
+//     Object.values(quiz[question]).forEach(option => {
+//         console.log(option);
+//     });
+// }
+
